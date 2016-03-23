817be9f0d1d886284bb7ccddfa417f816250c7e9
Victor Kayser <kayser.vic@gmail.com>
2016-03-23 15:52:07 +0100

    new background + gif

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,7 +34,7 @@
 		 * Background image
 		/* ---------------------------------------------- */
 
-		$('#intro').backstretch(['assets/images/background4.jpg']);
+		$('#intro').backstretch(['assets/images/background5.jpg']);
 
 		/* ---------------------------------------------- /*
 		 * Navbar
