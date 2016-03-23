254cd290046bcda66f2b5452d9834f06af0178c2
Victor Kayser <kayser.vic@gmail.com>
2016-03-23 17:27:43 +0100

    modifs

--- assets/php/download.php
+++ assets/php/download.php
@@ -3,7 +3,7 @@
 ignore_user_abort(true);
 set_time_limit(0); // disable the time limit for this script
 
-$fullPath = "../../CV-Victor-Kayser.pdf";
+$fullPath = "../../Victor-Kayser.pdf";
 
 if ($fd = fopen ($fullPath, "r")) {
     $fsize = filesize($fullPath);
